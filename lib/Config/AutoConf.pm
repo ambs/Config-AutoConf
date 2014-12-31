b91ecb53921c93218d88258551929492c2edeb36
Jens Rehsack <sno@netbsd.org>
2014-12-31 12:24:53 +0100

    bump version to 0.310 after release

--- lib/Config/AutoConf.pm
+++ lib/Config/AutoConf.pm
@@ -92,7 +92,7 @@ Config::AutoConf - A module to implement some of AutoConf macros in pure perl.
 
 =cut
 
-our $VERSION = '0.309';
+our $VERSION = '0.310';
 $VERSION = eval $VERSION;
 
 =head1 ABSTRACT
