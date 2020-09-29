7acb5b386469f68ecb344ccdaff7193a8869bdd8
Jens Rehsack <sno@netbsd.org>
2020-09-29 11:49:00 +0200

    {AutoConf.pm,Changes}: prepare for release
    Add noteworthy changes and bump version to release new version.
    
    Signed-off-by: Jens Rehsack <sno@netbsd.org>

--- lib/Config/AutoConf.pm
+++ lib/Config/AutoConf.pm
@@ -92,7 +92,7 @@ Config::AutoConf - A module to implement some of AutoConf macros in pure perl.
 
 =cut
 
-our $VERSION = '0.318';
+our $VERSION = '0.319';
 $VERSION = eval $VERSION;
 
 =head1 ABSTRACT
